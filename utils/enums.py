f26bde59f481849cfbb3145261007d262d3dacd6
Mauricio Aguilera <mauricio@familify.com>
2024-01-28 10:05:53 -0500

    change enums

--- utils/enums.py
+++ utils/enums.py
@@ -4,19 +4,19 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TaskPriority(TextChoices):
-    HIGH = 'highest', _('Highest')
-    HIGHEST = 'high', _('High')
-    MEDIUM = 'medium', _('Medium')
-    LOW = 'low', _('Low')
-    LOWEST = 'lowest', _('Lowest')
+    HIGH = 'highest', _('highest')
+    HIGHEST = 'high', _('high')
+    MEDIUM = 'medium', _('medium')
+    LOW = 'low', _('low')
+    LOWEST = 'lowest', _('lowest')
 
 
 class TaskStatus(TextChoices):
-    PENDING = 'pending', _('Pending')
-    IN_PROGRESS = 'in_progress', _('In Progress')
-    COMPLETED = 'completed', _('Completed')
+    PENDING = 'pending', _('pending')
+    IN_PROGRESS = 'in_progress', _('in_progress')
+    COMPLETED = 'completed', _('completed')
 
 
 class TaskType(TextChoices):
-    EMERGENCY = 'emergency', _('Emergency')
-    PREVENTIVE = 'preventive', _('Preventive')
+    EMERGENCY = 'emergency', _('emergency')
+    PREVENTIVE = 'preventive', _('preventive')
