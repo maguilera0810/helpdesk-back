54e096a57eafccc429e08db8b5e6d241fe6179a5
Mauricio Aguilera <mauricio@familify.com>
2024-01-27 22:37:20 -0500

    skill model

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,4 +1,5 @@
 # .\apps\core\models.py
+from collections.abc import Iterable
 from django.db import models
 
 
@@ -8,4 +9,11 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
- 
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
