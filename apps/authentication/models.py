ce84d5d1652d55900245416e698136612de62731
Mauricio Aguilera <mauricio@familify.com>
2024-06-10 22:35:52 -0500

    models auth

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,11 +1,22 @@
 # .\apps\authentication\models.py
+from typing import Union
+
 from django.db import models
 
 
 class Profile(models.Model):
+
     user = models.OneToOneField('auth.User', on_delete=models.DO_NOTHING)
     phone_number = models.CharField(max_length=15, blank=True)
     address = models.CharField(max_length=255, blank=True)
     is_available = models.BooleanField(default=True)
     external_code = models.CharField(max_length=50, blank=True)
     skills = models.ManyToManyField('common.Skill', related_name='profiles')
+
+
+MODELS = [
+    Profile,
+]
+MODELS_TYPES = Union[
+    Profile,
+]
