7028faccff8fbb9cd5bcf78596b8d2261cf5c3b9
Mauricio Aguilera <mauricio@familify.com>
2024-06-10 22:48:31 -0500

    init apps

---
+++ apps/__init__.py
@@ -0,0 +1,8 @@
+from apps.authentication.models import MODEL_TYPES as AUTH_MODEL_TYPES
+from apps.common.models import MODEL_TYPES as COMMON_MODEL_TYPES
+from apps.management.models import MODEL_TYPES as MANAGE_MODEL_TYPES
+
+MODEL_TYPES = AUTH_MODEL_TYPES | COMMON_MODEL_TYPES | MANAGE_MODEL_TYPES
+
+
+__all__ = ["MODEL_TYPES"]
