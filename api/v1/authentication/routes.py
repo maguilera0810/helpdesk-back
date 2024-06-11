55e220ce2e5b76bea467ffd71437fedd1d268910
Mauricio Aguilera <mauricio@familify.com>
2024-06-10 20:13:35 -0500

    delete files

--- api/v1/authentication/routes.py
+++
@@ -1,7 +0,0 @@
-# .\api\v1\authentication\routes.py
-from django.urls import path
-from v1.authentication.views.auth_view import AuthView
-
-urlpatterns = [
-    path("login/", AuthView.as_view({"post": "login"}), name="v1_login"),
-]
--- api/v1/routes.py
+++
@@ -1,8 +0,0 @@
-# .\api\v1\routes.py
-from django.urls import include, path
-
-urlpatterns = [
-    path('authentication/', include('api.v1.authentication.routes')),
-    path('common/', include('api.v1.common.routes')),
-    path('management/', include('api.v1.management.routes')),
-]
