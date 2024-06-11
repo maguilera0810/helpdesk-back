ef00dc49eb3e12d0fdebeabdfbad2af318d543b6
Mauricio Aguilera <mauricio@familify.com>
2024-06-10 20:12:59 -0500

    models core

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,6 +1,18 @@
 # .\apps\core\models.py
+from datetime import datetime
+
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
+
+from resources.enums import StoragePathEnum
+
+ENV = settings.ENV
+
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
 
 
 class AuditModel(models.Model):
@@ -9,6 +21,23 @@ class AuditModel(models.Model):
 
     class Meta:
         abstract = True
+
+    def time_since_created(self, format: str = "days"):
+        return self.__time_since(self.created_at, format)
+
+    def time_since_updated(self, format: str = "days"):
+        return self.__time_since(self.updated_at, format)
+
+    @classmethod
+    def __time_since(cls, timestamp: datetime, format: str = "days"):
+        if not timestamp:
+            return
+        now = timezone.now()
+        delta = now - timestamp
+        result = delta.days
+        if format == "hours":
+            result = result * 24 + (delta.seconds // 3600)
+        return result
 
 
 class PeriodModel(models.Model):
