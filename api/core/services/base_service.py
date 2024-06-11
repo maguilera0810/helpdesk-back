47f7c2701a5c92e336de6036afd8e166c61992fd
Mauricio Aguilera <mauricio@familify.com>
2024-06-10 22:21:05 -0500

    base srv

---
+++ api/core/services/base_service.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+
+from django.db.models import Model
+
+
+class BaseService(ABC):
+
+    @classmethod
+    @abstractmethod
+    def get_one(cls, id: int):
+        ...
+
+    @classmethod
+    @abstractmethod
+    def get_all(cls, incl_filters: dict = None, excl_filters: dict = None):
+        ...
+
+    @classmethod
+    @abstractmethod
+    def delete(cls, id: int):
+        ...
+
+    @classmethod
+    def delete_all(cls, incl_filters: dict = None, excl_filters: dict = None):
+        ...
+
+    @classmethod
+    def create_multiple(cls, data_list: list, delete_data: bool = False):
+        ...
+
+    @classmethod
+    def update_files(cls, files, instance: Model = None, id: int = None):
+        ...
+
+    @classmethod
+    def save_file(cls, file, instance: Model, attr: str):
+        ...
+
+    @classmethod
+    def set_translation_keys(
+        cls,
+        instance: Model = None,
+        id: int = None,
+        keys: list[str] = ["translation_key"],
+    ):
+        ...
