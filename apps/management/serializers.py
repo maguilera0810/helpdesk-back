ee4e02f56b76ccf9b8003fb8191b402593651225
Mauricio Aguilera <mauricio@familify.com>
2024-06-10 22:27:24 -0500

    serializers

--- apps/management/serializers.py
+++ apps/management/serializers.py
@@ -1,2 +1,48 @@
 # .\apps\management\serializers.py
 from rest_framework.serializers import ModelSerializer
+
+from apps.management.models import (MaintenancePlan, Report, Request,
+                                    RequestingUnit, ScheduledTask, Task,
+                                    TaskHistory)
+
+
+class MaintenancePlanSerializer(ModelSerializer):
+    class Meta:
+        model = MaintenancePlan
+        fields = "__all__"
+
+
+class ScheduledTaskSerializer(ModelSerializer):
+    class Meta:
+        model = ScheduledTask
+        fields = "__all__"
+
+
+class TaskSerializer(ModelSerializer):
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class RequestingUnitSerializer(ModelSerializer):
+    class Meta:
+        model = RequestingUnit
+        fields = "__all__"
+
+
+class RequestSerializer(ModelSerializer):
+    class Meta:
+        model = Request
+        fields = "__all__"
+
+
+class TaskHistorySerializer(ModelSerializer):
+    class Meta:
+        model = TaskHistory
+        fields = "__all__"
+
+
+class ReportSerializer(ModelSerializer):
+    class Meta:
+        model = Report
+        fields = "__all__"
