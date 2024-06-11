2f3dc2609e77ddc4fc4a7b10a2c2c765d81d9255
Mauricio Aguilera <mauricio@familify.com>
2024-06-10 22:34:58 -0500

    models common

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,7 +1,8 @@
 # .\apps\common\models.py
 from collections.abc import Iterable
-from django.db import models
+from typing import Union
 
+from django.db import models
 
 # class Tag(models.Model):
 #     name = models.CharField(max_length=100)
@@ -17,3 +18,11 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+
+
+MODELS = [
+    Skill,
+]
+MODELS_TYPES = Union[
+    Skill,
+]
