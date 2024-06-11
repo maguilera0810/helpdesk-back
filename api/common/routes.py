0a948e9bb2117cd874104edff8824e1cc7a76bd1
Mauricio Aguilera <mauricio@familify.com>
2024-06-10 22:55:15 -0500

    routes common

---
+++ api/common/routes.py
@@ -0,0 +1,13 @@
+# .\api\v1\common\routes.py
+from django.urls import path
+
+from api.common.views.skill_view import SkillView
+
+urlpatterns = [
+    # SKILL
+    path('skill/', SkillView.as_view({"get": "list",
+                                      "post": "create"}), name="skill"),
+    path('skill/<int:id>', SkillView.as_view({"get": "retrieve",
+                                              "put": "update",
+                                              "delete": "destroy"}), name="skill_id"),
+]
