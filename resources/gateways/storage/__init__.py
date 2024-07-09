cfa82913ab1875c23982ed127e121e8c7c5aa5b0
Mauricio Aguilera <mauricio@familify.com>
2024-07-09 13:26:47 -0500

    delete unnecessary files

--- resources/gateways/storage/__init__.py
+++
@@ -1,9 +0,0 @@
-# .\resources\gateways\storage\__init__.py
-from resources.gateways.storage.base_storage_gateway import BaseStorageGateway
-from resources.gateways.storage.s3_gateway import S3Gateway
-
-
-__all__ = [
-    "BaseStorageGateway",
-    "S3Gateway",
-]
--- resources/gateways/translation/__init__.py
+++
@@ -1,9 +0,0 @@
-# .\resources\gateways\translation\__init__.py
-from resources.gateways.translation.base_translation_gateway import BaseTranslationGateway
-from resources.gateways.translation.weblate_gateway import WeblateGateway
-
-
-__all__ = [
-    "BaseTranslationGateway",
-    "WeblateGateway",
-]
