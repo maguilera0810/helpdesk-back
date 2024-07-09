a6e04f217c9646ddfcb75718dc32adbd48f9022b
Mauricio Aguilera <mauricio@familify.com>
2024-07-09 13:27:36 -0500

    init files

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,9 +1,1 @@
 # .\apps\__init__.py
-from apps.authentication.models import MODEL_TYPES as AUTH_MODEL_TYPES
-from apps.common.models import MODEL_TYPES as COMMON_MODEL_TYPES
-from apps.management.models import MODEL_TYPES as MANAGE_MODEL_TYPES
-
-MODEL_TYPES = AUTH_MODEL_TYPES | COMMON_MODEL_TYPES | MANAGE_MODEL_TYPES
-
-
-__all__ = ["MODEL_TYPES"]
--- resources/factories/__init__.py
+++ resources/factories/__init__.py
@@ -1,8 +1,1 @@
 # .\resources\factories\__init__.py
-from resources.factories.storage_factory import StorageFactory
-from resources.factories.translation_factory import TranslationFactory
-
-__all__ = [
-    "StorageFactory",
-    "TranslationFactory",
-]
