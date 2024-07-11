c15fc38dbf0e93bd844744c7ab775cf22068f837
Mauricio Aguilera <mauricio@familify.com>
2024-07-11 13:30:42 -0500

    enums

--- resources/enums.py
+++ resources/enums.py
@@ -26,7 +26,7 @@ class DocumentTypeEnum(TextChoices):
     PASSPORT = "passport", _("passport")
 
 
-class AuthErrorEnum(TextChoices):
+class AuthMsgEnum(TextChoices):
     USER_ALREADY_EXIST = "user_already_exist", _("user_already_exist")
     USER_DOES_NOT_EXIST = "user_does_not_exist", _("user_does_not_exist")
     EMAIL_ALREADY_EXIST = "email_already_exist", _("email_already_exist")
@@ -34,8 +34,9 @@ class AuthErrorEnum(TextChoices):
         "document_already_exist")
 
 
-class ValidatorErrorEnum(TextChoices):
+class ValidatorMsgEnum(TextChoices):
     EMAIL_ERROR = "email_error", _("email_error")
+    EMAIL_OK = "email_ok", _("email_ok")
     PASSWORD_LENGTH_ERROR = "password_length_error", _("password_length_error")
     PASSWORD_UPPERCASE_ERROR = "password_uppercase_error", _(
         "password_uppercase_error")
@@ -44,4 +45,7 @@ class ValidatorErrorEnum(TextChoices):
     PASSWORD_NUMBER_ERROR = "password_number_error", _("password_number_error")
     PASSWORD_SPECIAL_CHAR_ERROR = "password_special_char_error", _(
         "password_special_char_error")
+    PASSWORD_OK = "password_ok", _("password_ok")
     PHONE_ERROR = "phone_error", _("phone_error")
+    PHONE_OK = "phone_ok", _("phone_ok")
+    ALL_OK = "all_ok", _("all_ok")
