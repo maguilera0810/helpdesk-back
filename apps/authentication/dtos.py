a9130e670dfaeaf27f71bbe7824499187e2bb1e2
Mauricio Aguilera <mauricio@familify.com>
2024-07-11 13:16:53 -0500

    dtos

--- apps/authentication/dtos.py
+++ apps/authentication/dtos.py
@@ -8,10 +8,15 @@ class AuthDTO:
     """
     Data transfer object for authentication details.
     """
-    email: str
-    password: str
-    first_name: str
-    last_name: str
+    email: Optional[str] = None
+    password: Optional[str] = None
+    phone: Optional[str] = None
+    address: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
     document_type: Optional[str] = None
     document: Optional[str] = None
-    phone: Optional[str] = None
+    is_staff: Optional[bool] = None
+    is_active: Optional[bool] = None
+    is_superuser: Optional[bool] = None
+    is_available: Optional[bool] = None
