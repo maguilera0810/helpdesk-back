f59bf2255d7841fd9f07e97251ddce4f2e9366e7
Mauricio Aguilera <mauricio@familify.com>
2024-07-11 12:08:45 -0500

    changte enums

--- resources/enums.py
+++ resources/enums.py
@@ -1,31 +1,47 @@
-# .\resources\enums.py
-from django.db.models import TextChoices
 from django.utils.translation import gettext_lazy as _
+from django.db.models import TextChoices
 
 
 class TaskPriorityEnum(TextChoices):
-    highest = "highest", _("highest")
-    high = "high", _("high")
-    medium = "medium", _("medium")
-    low = "low", _("low")
-    lowest = "lowest", _("lowest")
+    HIGHEST = "highest", _("highest")
+    HIGH = "high", _("high")
+    MEDIUM = "medium", _("medium")
+    LOW = "low", _("low")
+    LOWEST = "lowest", _("lowest")
 
 
 class TaskStatusEnum(TextChoices):
-    pending = "pending", _("pending")
-    in_progress = "in_progress", _("in_progress")
-    completed = "completed", _("completed")
+    PENDING = "pending", _("pending")
+    IN_PROGRESS = "in_progress", _("in_progress")
+    COMPLETED = "completed", _("completed")
 
 
 class TaskTypeEnum(TextChoices):
-    emergency = "emergency", _("emergency")
-    preventive = "preventive", _("preventive")
+    EMERGENCY = "emergency", _("emergency")
+    PREVENTIVE = "preventive", _("preventive")
 
 
 class DocumentTypeEnum(TextChoices):
-    id_card = "id_card", _("id_card")
-    passport = "passport", _("passport")
+    ID_CARD = "id_card", _("id_card")
+    PASSPORT = "passport", _("passport")
 
 
 class AuthErrorEnum(TextChoices):
-    user_already_exist = "user_already_exist", _("user_already_exist")
+    USER_ALREADY_EXIST = "user_already_exist", _("user_already_exist")
+    USER_DOES_NOT_EXIST = "user_does_not_exist", _("user_does_not_exist")
+    EMAIL_ALREADY_EXIST = "email_already_exist", _("email_already_exist")
+    DOCUMENT_ALREADY_EXIST = "document_already_exist", _(
+        "document_already_exist")
+
+
+class ValidatorErrorEnum(TextChoices):
+    EMAIL_ERROR = "email_error", _("email_error")
+    PASSWORD_LENGTH_ERROR = "password_length_error", _("password_length_error")
+    PASSWORD_UPPERCASE_ERROR = "password_uppercase_error", _(
+        "password_uppercase_error")
+    PASSWORD_LOWERCASE_ERROR = "password_lowercase_error", _(
+        "password_lowercase_error")
+    PASSWORD_NUMBER_ERROR = "password_number_error", _("password_number_error")
+    PASSWORD_SPECIAL_CHAR_ERROR = "password_special_char_error", _(
+        "password_special_char_error")
+    PHONE_ERROR = "phone_error", _("phone_error")
