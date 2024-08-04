6263baaa9ce264d55e144254fd76c1fd447be01b
Mauricio Aguilera <mauricio@familify.com>
2024-08-04 12:28:41 -0500

    routes core

--- api/core/routes.py
+++ api/core/routes.py
@@ -1,3 +1,20 @@
-METHODS = {"get": "list", "post": "create"}
-METHODS_ID = {"get": "retrieve", "put": "update", "delete": "destroy"}
-METHODS_FILE = {"put": "update_files"}
+from django.urls import path
+from django.utils.text import slugify
+from api.core.views.base_crud_view import BaseCRUDView
+METHODS = {
+    "get": "list",
+    "post": "create",
+}
+METHODS_ID = {
+    "get": "retrieve",
+    "put": "update",
+    "delete": "destroy",
+}
+
+
+def get_crud_route(model: str, view: BaseCRUDView):
+    model = slugify(model)
+    return [
+        path(f"{model}/", view.as_view({**METHODS})),
+        path(f"{model}/<int:id>/", view.as_view({**METHODS_ID})),
+    ]
