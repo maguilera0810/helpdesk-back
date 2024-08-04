658cfcbe4d21b715ced2e5a980e2f6a66426641b
Mauricio Aguilera <mauricio@familify.com>
2024-08-04 16:53:51 -0500

    fix local setting

--- settings/local.py
+++ settings/local.py
@@ -3,7 +3,7 @@ from settings.base import *
 
 DEBUG = config("DEBUG", cast=bool, default=True)
 
-USE_BASIC_AUTH = onfig("USE_BASIC_AUTH", cast=bool, default=True)
+USE_BASIC_AUTH = config("USE_BASIC_AUTH", cast=bool, default=True)
 if USE_BASIC_AUTH:
     SWAGGER_SETTINGS["SECURITY_DEFINITIONS"]["Basic"] = {
         "type": "basic",
