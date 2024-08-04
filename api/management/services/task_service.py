88068d74a1f9b033bf446f71dc2c6eb2e739b353
Mauricio Aguilera <mauricio@familify.com>
2024-08-04 14:37:48 -0500

    task srv

--- api/management/services/task_service.py
+++ api/management/services/task_service.py
@@ -1,6 +1,14 @@
 from api.core.services.base_crud_service import BaseCRUDService
 from apps.management.models import Task
+from django.db import transaction
+from django.contrib.auth.models import User
+from apps.authentication.dtos import AuthDTO
 
 
 class TaskService(BaseCRUDService):
     model = Task
+
+    @classmethod
+    @transaction.atomic
+    def create_task(cls, user: User, data):
+        ...
