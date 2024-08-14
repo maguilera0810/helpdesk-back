86bd0dfd2878d2db5c6c94dc955288f80588402e
Mauricio Aguilera <mauricio@familify.com>
2024-08-14 16:12:20 -0500

    import signals

--- apps/management/apps.py
+++ apps/management/apps.py
@@ -1,9 +1,10 @@
-# .\apps\management\apps.py
-# apps\management\apps.py
 from django.apps import AppConfig
 
 
 class ManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.management'
-    label = 'management'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.management"
+    label = "management"
+
+    def ready(self):
+        import apps.management.signals  # noqa: F401
