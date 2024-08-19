2228cbe00c82c156d546046213ac50352089bb5d
Mauricio Aguilera <mauricio@familify.com>
2024-08-18 22:50:18 -0500

    task view

--- api/management/views/task_view.py
+++ api/management/views/task_view.py
@@ -26,70 +26,10 @@ class TaskView(BaseCRUDView, IsAuthenticatedView):
     @schema(action="create")
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
-        """
-        Method: POST
-
-        Request:
-            - title (str)
-            - description (str)
-            - code (str)
-            - type (str)
-            - status 
-            - priority
-            - created_by: por el token de usuario
-            - responsible
-            - team
-            - plan
-            - scheduled
-            - date_execution TODO
-            - interval TODO
-        """
-        data = request.data
-        user = request.user
-
-        serializer = self.serial_class(data=data,
-                                       partial=True,
-                                       context={"request_user": user})
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @schema(action="update")
-    def update(self, request, id, *args, **kwargs):
+    def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
-        """
-        Method: PUT
-
-        Request:
-            - title (str)
-            - description (str)
-            - code (str)
-            - type (str)
-            - status 
-            - priority
-            - created_by: por el token de usuario
-            - responsible
-            - team
-            - plan
-            - scheduled
-            - date_execution TODO
-            - interval TODO
-        """
-        data = request.data
-        user = request.user
-
-        if not (task := self.srv_class.get_one(id=id)):
-            return Response({}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.serial_class(task,
-                                       data=data,
-                                       partial=True,
-                                       context={"request_user": user})
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @schema(action="destroy")
     def destroy(self, request, *args, **kwargs):
