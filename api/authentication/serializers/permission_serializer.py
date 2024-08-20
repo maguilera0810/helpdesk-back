95bc90663a794c265c7249981bb32769a8637117
Mauricio Aguilera <mauricio@familify.com>
2024-08-19 23:17:11 -0500

    permission crud

---
+++ api/authentication/serializers/permission_serializer.py
@@ -0,0 +1,10 @@
+# .\api\authentication\serializers\auth_serializers.py
+from django.contrib.auth.models import Permission
+from rest_framework.serializers import ModelSerializer
+
+
+class PermissionSerializer(ModelSerializer):
+
+    class Meta:
+        model = Permission
+        fields = "__all__"
