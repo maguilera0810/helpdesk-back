9e667baed7dfd9a1f12d4785ed8c05a8de175fa9
Mauricio Aguilera <mauricio@familify.com>
2024-08-21 22:00:22 -0500

    issue serializer

--- api/management/serializers/issue_serializer.py
+++ api/management/serializers/issue_serializer.py
@@ -1,32 +1,17 @@
 from django.db import transaction
-from rest_framework.serializers import ModelSerializer, ValidationError
 
+from api.core.serializers.base_serializer import BaseSerializer
 from apps.management.models import Issue
 
 
-class IssueSerializer(ModelSerializer):
+class IssueSerializer(BaseSerializer):
 
     class Meta:
         model = Issue
         fields = "__all__"
-        # read_only_fields = ()
-
-    def validate_request_user(self):
-        request = self.context.get("request")
-        if not request:
-            raise ValidationError({"request": "no_request"})
-        request_user = request.user
-        if not request_user:
-            raise ValidationError({"user": "no_request_user"})
-        return request_user
-
-    def validate(self, data: dict):
-        self.validate_request_user()
-        return data
 
     @transaction.atomic
     def save(self, **kwargs):
-
         if not self.instance:
             request = self.context.get("request")
             self.validated_data["created_by"] = request.user
