8272c6591c7a64a737d870097a9ff018a1eaaaba
Mauricio Aguilera <mauricio@familify.com>
2024-08-23 17:44:29 -0500

    routes management common

--- api/common/routes.py
+++ api/common/routes.py
@@ -1,9 +1,13 @@
 # .\api\common\routes.py
 from django.urls import path
-from api.core.routes import get_crud_route
+
+from api.common.views.category_view import CategoryView
 from api.common.views.skill_view import SkillView
+from api.common.views.tag_view import TagView
+from api.core.routes import get_crud_route
 
 urlpatterns = [
-    # PLAN
+    *get_crud_route("category", CategoryView),
     *get_crud_route("skill", SkillView),
+    *get_crud_route("tag", TagView),
 ]
--- api/management/routes.py
+++ api/management/routes.py
@@ -6,12 +6,8 @@ from api.management.views.plan_view import PlanView
 from api.management.views.task_view import TaskView
 
 urlpatterns = [
-    # PLAN
     *get_crud_route("plan", PlanView),
-    # TASK
     *get_crud_route("task", TaskView),
-    # ISSUE
     *get_crud_route("issue", IssueView),
-    # ISSUE_FILE
     *get_crud_route("issue-file", IssueFileView),
 ]
