233adc8e471e544882cd90752ec6c5569bca22a3
Mauricio Aguilera <mauricio@familify.com>
2024-09-25 19:30:02 -0500

    control collisions

--- api/management/services/task_service.py
+++ api/management/services/task_service.py
@@ -55,7 +55,9 @@ class TaskService(BaseCRUDService):
         return None, serializer.errors
 
     @classmethod
-    def retrieve_schedules(cls, responsible_id: int, team: list[int], start_at: datetime, end_at: datetime):
+    def retrieve_schedule(cls, responsible_id: int, team: list[int],
+                          start_at: datetime, end_at: datetime,
+                          curr_task_id: int = None):
 
         if not isinstance(start_at, datetime):
             start_at = datetime.strptime(start_at, DATE_FORMAT)
@@ -65,8 +67,8 @@ class TaskService(BaseCRUDService):
         start_at = timezone.make_aware(start_at, timezone=timezone.utc)
         end_at = timezone.make_aware(end_at, timezone=timezone.utc)
 
-        start_at = start_at.astimezone(LOCAL_TZ)
-        end_at = end_at.astimezone(LOCAL_TZ)
+        # start_at = start_at.astimezone(LOCAL_TZ)
+        # end_at = end_at.astimezone(LOCAL_TZ)
 
         start_date = start_at.date()
         end_date = end_at.date()
@@ -74,18 +76,33 @@ class TaskService(BaseCRUDService):
         responsible_tasks = cls.__query_tasks(user_id=responsible_id,
                                               start_date=start_date,
                                               end_date=end_date)
-        schedules = [{"user_id": responsible_id,
+        has_collision = False
+        for task in responsible_tasks:
+            collition = cls.__check_task_collision(curr_task_id=curr_task_id,
+                                                   task=task,
+                                                   start_at=start_at,
+                                                   end_at=end_at)
+            has_collision = has_collision or collition
+        user_tasks = [{"user_id": responsible_id,
                       "tasks": responsible_tasks}]
 
         team = [t for t in team if t != responsible_id]
         for user_id in team:
-            user_tasks = cls.__query_tasks(user_id=user_id,
+            team_tasks = cls.__query_tasks(user_id=user_id,
                                            start_date=start_date,
                                            end_date=end_date)
-            schedules.append({"user_id": user_id,
-                              "tasks": user_tasks})
-
-        return schedules
+            for task in team_tasks:
+                collition = cls.__check_task_collision(curr_task_id=curr_task_id,
+                                                       task=task,
+                                                       start_at=start_at,
+                                                       end_at=end_at)
+                has_collision = has_collision or collition
+            user_tasks.append({"user_id": user_id,
+                              "tasks": team_tasks})
+        return {
+            "has_collision": has_collision,
+            "user_tasks": user_tasks
+        }
 
     @classmethod
     def __query_tasks(cls, user_id, start_date, end_date):
@@ -94,3 +111,9 @@ class TaskService(BaseCRUDService):
             .filter(Q(start_at__date__gte=start_date, start_at__date__lte=end_date) |
                     Q(end_at__date__gte=start_date, end_at__date__lte=end_date))\
             .values("id", "title", "start_at", "end_at").order_by("start_at").distinct()
+
+    @classmethod
+    def __check_task_collision(cls, curr_task_id: int, task: dict, start_at: datetime, end_at: datetime):
+        task["has_collision"] = (task["id"] != curr_task_id and
+                                 task["start_at"] <= end_at and
+                                 task["end_at"] <= start_at)
