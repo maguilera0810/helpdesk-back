aaf8f137105e87cc8f5b78a6b729f789e79e0620
Mauricio Aguilera <mauricio@familify.com>
2024-09-29 16:32:58 -0500

    category type crud

--- api/common/routes.py
+++ api/common/routes.py
@@ -1,6 +1,7 @@
 # .\api\common\routes.py
 from django.urls import path
 
+from api.common.views.category_type_view import CategoryTypeView
 from api.common.views.category_view import CategoryView
 from api.common.views.skill_view import SkillView
 from api.common.views.tag_view import TagView
@@ -8,6 +9,7 @@ from api.core.routes import get_crud_route
 
 urlpatterns = [
     *get_crud_route("category", CategoryView),
+    *get_crud_route("category-type", CategoryTypeView),
     *get_crud_route("skill", SkillView),
     *get_crud_route("tag", TagView),
 ]
