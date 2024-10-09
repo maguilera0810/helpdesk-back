d9ecb472e6430912a1e1c7a80aa4bbeabcf19b33
Mauricio Aguilera <mauricio@familify.com>
2024-10-08 20:37:13 -0500

    validators

---
+++ apps/core/validators.py
@@ -0,0 +1,6 @@
+# .\apps\common\validators.py
+from django.core.validators import RegexValidator
+
+color_validator = RegexValidator(regex=r"^#[0-9A-Fa-f]{6}$",
+                                 message="El color debe ser un valor hexadecimal válido",
+                                 code="invalid_color")
