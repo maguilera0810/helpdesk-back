e7ad2cabde6180553feae16e1e30630fb108dcc4
Mauricio Aguilera <mauricio@familify.com>
2024-10-27 09:09:46 -0500

    chnage filters

--- resources/utils/filter_util.py
+++ resources/utils/filter_util.py
@@ -1,23 +1,31 @@
 # .\resources\utils\filter_util.py
+
 class FilterUtil:
+
     @staticmethod
-    def get_list_filters(data: dict[str, str]) -> dict[dict]:
+    def get_list_filters(data: dict[str, str]) -> dict[str, dict]:
         if not data:
             return {}
-        incl_prefix = "incl_"
-        excl_prefix = "excl_"
-        list_sufix = "__in"
-        incl_filters = {}
-        excl_filters = {}
-        for k, v in data.items():
-            is_list = k.endswith(list_sufix)
-            if k.startswith(excl_prefix):
-                key = k.split(excl_prefix)[-1]
-                excl_filters[key] = v.split(",") if is_list else v
-            elif k.startswith(incl_prefix):
-                key = k.split(incl_prefix)[-1]
-                incl_filters[key] = v.split(",") if is_list else v
+
+        incl_prefix, excl_prefix, order_key, list_suffix = "incl_", "excl_", "order_by", "__in"
+        incl_filters, excl_filters, order = {}, {}, []
+
+        if v := data.get(order_key):
+            order = v.split(",") if "," in v else [v]
+            del data[order_key]
+
+        for key, v in data.items():
+            values = v.split(",") if key.endswith(
+                list_suffix) or "," in v else v
+
+            if key.startswith(excl_prefix):
+                excl_filters[key.removeprefix(excl_prefix)] = values
+            elif key.startswith(incl_prefix):
+                incl_filters[key.removeprefix(incl_prefix)] = values
             else:
-                key = k
-                incl_filters[key] = v.split(",") if is_list else v
-        return {"incl_filters": incl_filters, "excl_filters": excl_filters}
+                incl_filters[key] = values
+        return {
+            "incl_filters": incl_filters,
+            "excl_filters": excl_filters,
+            "order": order,
+        }
