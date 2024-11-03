4c9d41ddf3111b5bedccd0040bc3d9d694f63854
Mauricio Aguilera <mauricio@familify.com>
2024-11-03 17:42:17 -0500

    routes api

--- api/routes.py
+++ api/routes.py
@@ -2,6 +2,7 @@
 from django.urls import include, path
 
 urlpatterns = [
+    path("analytics/", include("api.analytics.routes")),
     path("auth/", include("api.authentication.routes")),
     path("common/", include("api.common.routes")),
     path("management/", include("api.management.routes")),
