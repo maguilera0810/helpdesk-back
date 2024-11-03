0d613b1764a4a27cfcd2c0fc03ef809009db99a2
Mauricio Aguilera <mauricio@familify.com>
2024-11-02 20:37:30 -0500

    apps analytics

--- apps/analytics/apps.py
+++ apps/analytics/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
-class AnalyticsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'analytics'
+class ManagementConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.analytics"
+    label = "analytics"
+
+    def ready(self):
+        import apps.core.signals  # noqa: F401
