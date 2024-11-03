58fea8f065c89dd9110a81ea1aa6a632e21683b8
Mauricio Aguilera <mauricio@familify.com>
2024-11-03 17:42:03 -0500

    enums periods

--- resources/enums.py
+++ resources/enums.py
@@ -83,3 +83,14 @@ class StoragePathEnum(TextChoices):
     state_animation = f"{state}/animation.<format>"
     store = f"{base}/store/<store_id>"
     store_icon = f"{store}/icon.<format>"
+
+
+class PeriodEnum(TextChoices):
+    today = "today", _("today")
+    yesterday = "yesterday", _("yesterday")
+    last_7_days = "last_7_days", _("last_7_days")
+    last_14_days = "last_14_days", _("last_14_days")
+    last_month = "last_month", _("last_month")
+    last_3_months = "last_3_months", _("last_3_months")
+    current_year = "current_year", _("current_year")
+    last_5_years = "last_5_years", _("last_5_years")
