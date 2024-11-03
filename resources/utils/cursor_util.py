7c280dedbd58008b6e7f9f27bdd3729a5bf6b6e9
Mauricio Aguilera <mauricio@familify.com>
2024-11-03 17:41:37 -0500

    cursor

--- resources/utils/cursor_util.py
+++ resources/utils/cursor_util.py
@@ -1,6 +1,8 @@
 # .\resources\utils\cursor_util.py
 from django.db import connections
 
+from resources.enums import PeriodEnum
+
 
 class CursorUtil:
 
@@ -9,7 +11,35 @@ class CursorUtil:
         cursor = connections[db].cursor()
         try:
             cursor.execute(query, params)
-            resp = cursor.fetchall() if try_fetch else None
-            return resp
+            if try_fetch:
+                return cursor.fetchall()
         finally:
             cursor.close()
+
+    @classmethod
+    def get_issue_status(cls, period: PeriodEnum):
+        query = """
+        select
+            ispv.status,
+            ispv.count
+        from
+            issue_status_periods_view ispv
+        where
+            ispv."period" = %s;
+        """
+        return cls.execute(query=query,
+                           params=[period])
+
+    @classmethod
+    def get_task_status(cls, period: PeriodEnum):
+        query = """
+        select
+            tspv.status,
+            tspv.count
+        from
+            task_status_periods_view tspv
+        where
+            tspv."period" = %s;
+        """
+        return cls.execute(query=query,
+                           params=[period])
